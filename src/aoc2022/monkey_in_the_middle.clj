dd78f59078d3ff4129477135f6e5d10b41acf94c
Stephen Allison <git@steve.allison.clara.co.uk>
2022-12-11 23:48:04 +0000

    Day 11 - Monkey in the Middle, part two, number theory to the resue

--- src/aoc2022/monkey_in_the_middle.clj
+++ src/aoc2022/monkey_in_the_middle.clj
@@ -2,21 +2,25 @@
 
 (def sample-monkeys
      (sorted-map
-       0 {:items [79 98] :op #(* % 19) :test [23 2 3] :inspections 0}
-       1 {:items [54 65 75 74] :op #(+ % 6) :test [19 2 0] :inspections 0}
-       2 {:items [79 60 97] :op #(* % %) :test [13 1 3] :inspections 0}
-       3 {:items [74] :op #(+ % 3) :test [17 0 1] :inspections 0}))
+       0 {:items (map bigint [79 98]) :op #(* % 19) :test [23 2 3] :inspections 0}
+       1 {:items (map bigint [54 65 75 74]) :op #(+ % 6) :test [19 2 0] :inspections 0}
+       2 {:items (map bigint [79 60 97]) :op #(* % %) :test [13 1 3] :inspections 0}
+       3 {:items (map bigint [74]) :op #(+ % 3) :test [17 0 1] :inspections 0}))
 
 (def actual-monkeys
      (sorted-map
-       0 {:items [50 70 54 83 52 78] :op #(* % 3) :test [11 2 7] :inspections 0}
-       1 {:items [71 52 58 60 71] :op #(* % %) :test [7 0 2] :inspections 0}
-       2 {:items [66 56 56 94 60 86 73] :op #(+ % 1) :test [3 7 5] :inspections 0}
-       3 {:items [83 99] :op #(+ % 8) :test [5 6 4] :inspections 0}
-       4 {:items [98 98 79] :op #(+ % 3) :test [17 1 0] :inspections 0}
-       5 {:items [76] :op #(+ % 4) :test [13 6 3] :inspections 0}
-       6 {:items [52 51 84 54] :op #(* % 17) :test [19 4 1] :inspections 0}
-       7 {:items [82 86 91 79 94 92 59 94] :op #(+ % 7) :test [2 5 3] :inspections 0}))
+       0 {:items (map bigint [50 70 54 83 52 78]) :op #(* % 3) :test [11 2 7] :inspections 0}
+       1 {:items (map bigint [71 52 58 60 71]) :op #(* % %) :test [7 0 2] :inspections 0}
+       2 {:items (map bigint [66 56 56 94 60 86 73]) :op #(+ % 1) :test [3 7 5] :inspections 0}
+       3 {:items (map bigint [83 99]) :op #(+ % 8) :test [5 6 4] :inspections 0}
+       4 {:items (map bigint [98 98 79]) :op #(+ % 3) :test [17 1 0] :inspections 0}
+       5 {:items (map bigint [76]) :op #(+ % 4) :test [13 6 3] :inspections 0}
+       6 {:items (map bigint [52 51 84 54]) :op #(* % 17) :test [19 4 1] :inspections 0}
+       7 {:items (map bigint [82 86 91 79 94 92 59 94]) :op #(+ % 7) :test [2 5 3] :inspections 0}))
+
+(def sample-super-divisor (* 23 19 13 17))
+(def actual-super-divisor (* 11 7 3 5 17 13 19 2))
+(def super-divisor actual-super-divisor)
 
 (defn remove-first [pred coll n]
       (loop [new []
@@ -30,7 +34,7 @@
 
 (defn remove-item [monkeys monkey-name item]
       (let [old-items (get-in monkeys [monkey-name :items])]
-           (assoc-in monkeys [monkey-name :items] (remove-first #(= % item) old-items item))))
+           (assoc-in monkeys [monkey-name :items] (remove #(= % item) old-items))))
 
 (defn add-item [monkeys monkey-name item]
       (let [old-items (get-in monkeys [monkey-name :items])]
@@ -43,7 +47,7 @@
           (update-in [from :inspections] inc)))
 
 (defn inspect-item [{:keys [test op]} item]
-      (let [new-item (Math/floorDiv ^long (op item) 3)
+      (let [new-item (op item)
             throw-to (test-item test new-item)]
            [throw-to new-item item]))
 
@@ -54,7 +58,7 @@
       (let [monkey (get monkeys monkey-name)
             item-throws (inspect monkey)]
            (reduce (fn [ms [receiver new-item old-item]]
-                       (throw-item ms monkey-name receiver new-item old-item))
+                       (throw-item ms monkey-name receiver (mod new-item super-divisor) old-item))
                    monkeys
                    item-throws)))
 
@@ -62,9 +66,16 @@
       (reduce (fn [ms name] (monkey-turn ms name)) monkeys (sort (keys monkeys))))
 
 (defn play-rounds [n monkeys]
-      (last (take (inc n) (iterate monkey-round monkeys))))
+      (println monkeys)
+      (loop [rounds-left n
+             state monkeys]
+            (println rounds-left)
+            (if (zero? rounds-left)
+                state
+                (recur (dec rounds-left) (monkey-round state)))))
 
 (defn solve []
       (let [final-state (play-rounds 10000 actual-monkeys)
             inspection-counts (reverse (sort (map :inspections (vals final-state))))]
+           (println final-state)
            (apply * (take 2 inspection-counts))))
